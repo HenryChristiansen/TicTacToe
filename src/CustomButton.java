8bb5a0b2fd4ab818c271533211b5f75d3c13e899
HenryChristiansen <61328987+HenryChristiansen@users.noreply.github.com>
2022-03-13 14:54:57 -0600

    Create CustomButton.java

---
+++ src/CustomButton.java
@@ -0,0 +1,74 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+
+@SuppressWarnings("serial")
+public class CustomButton extends JButton {
+	Font font;
+	String status;
+	boolean isEmpty;
+
+	CustomButton() {
+		isEmpty = true;
+		setVisible(true);
+		font = new Font("Garamond", Font.PLAIN, 100);
+		this.setFont(font);
+		this.setBorder(BorderFactory.createLineBorder(Color.white, 2));
+		this.setBackground(Color.black);
+		this.setForeground(Color.white);
+		this.setContentAreaFilled(false);
+		this.setFocusable(false);
+		setText(" ");
+
+		this.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (isEmpty && !TicTacToe.singlePlayer && TicTacToe.play) {
+					if (TicTacToe.xTurn) {
+						isEmpty = false;
+						setText("X");
+						TicTacToe.xTurn = false;
+						synchronized (TicTacToe.syncObject) {
+							TicTacToe.syncObject.notify();
+						}
+						TicTacToe.changeTurnText('O');
+					} else {
+						isEmpty = false;
+						setText("O");
+						TicTacToe.xTurn = true;
+						synchronized (TicTacToe.syncObject) {
+							TicTacToe.syncObject.notify();
+						}
+						TicTacToe.changeTurnText('X');
+					}
+				}
+
+				if (isEmpty && TicTacToe.singlePlayer && TicTacToe.turn.getText().equals("X's Turn")
+						&& TicTacToe.play) {
+					setText("X");
+					isEmpty = false;
+					synchronized (TicTacToe.syncObject) {
+						TicTacToe.syncObject.notify();
+					}
+				}
+
+			}
+		});
+	}
+
+	public void computerGoHere() {
+		this.setText("O");
+	}
+	
+	public void setEmpty(boolean b) {
+		this.isEmpty = b;
+	}
+
+	public void setWinBorder(int size) {
+		this.setBorder(BorderFactory.createLineBorder(Color.red, size));
+	}
+
+}
